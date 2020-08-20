24ca650a315bb0e22c767a461c0198955cbdf01f
Michał Nowak <michal.nowak061@gmail.com>
2020-08-20 12:52:36 +0200

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Michał Nowak on 20/08/2020.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
